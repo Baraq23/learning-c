bf2efd999ea91b490c3ae7d1e89b1092ccd1cbed
barraotieno <barrackotieno160@gmail.com>
2025-03-12 17:14:04 +0300

    fix; remove '\n' character in entered name.

---
+++ c-basics/input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    int age;
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name)-1] = '\0';
+    printf("What's your age?: ");
+    scanf("%d", &age);
+
+    printf("Hi %s, we recorded your age as: %d\n\n", name, age);
+
+    return 0;
+}
---
+++ c-basics/variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int age = 26;
+    float height = 9.28;
+    char gender = 'M';
+
+    printf("The user's age is %d\n", age);
+    printf("The user's height is %.1f\n", height);
+    printf("The user's gender is %c\n", gender);
+    printf("\n");
+
+
+    return 0;
+}
