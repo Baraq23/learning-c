28f28739882606909019c4da7498db9073617064
barraotieno <barrackotieno160@gmail.com>
2025-03-17 11:48:30 +0300

    feat; implement age classification based on user input

---
+++ c-basics/if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("What's your age?: ");
+    scanf("%d", &age);
+    if (age <= 12) {
+        printf("You are a minor\n");
+    } else if (age > 12 && age <= 18) {
+        printf("You are a teenager.\n");
+    } else if (age > 18 && age <= 59) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a senior citizen.\n");
+    }
+    return 0;
+}
